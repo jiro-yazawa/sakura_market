84c364e4b004a551946502c913e2a4910f8c2fb4
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-08-07 14:49:22 +0900

    deviseを導入してログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {sessions: "users/sessions"}
   root to: "items#index"
-  resources :items, only: :show
+  resources :items, only: [:show, :new, :create, :destroy]
 end
