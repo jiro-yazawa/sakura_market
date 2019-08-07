ae05a2b5092ad5102748beb9a7e495c649c302da
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-08-07 18:09:42 +0900

    商品一覧画面と詳細画面の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,11 @@ class ItemsController < ApplicationController
   before_action :admin_user, only: [:new, :create, :destoroy]
 
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
