d25b8f037b50e4bb5cb3f41c6febb77235aeee82
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-08-10 19:21:40 +0900

    注文機能と注文履歴の一覧画面を作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,18 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @orders = current_user.orders
+  end
+
+  def show
+    @order = current_user.orders.find(1)
+  end
+
   def create
     @order = current_user.orders.new(order_params)
+    @order.user_name = current_user.address.name
+    @order.user_location = current_user.address.location
     if @order.save
       redirect_to root_url, notice: '注文に成功しました。'
     else
