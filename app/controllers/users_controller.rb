4e302459553ebd153191b0c614ce0da29fbee3c6
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-08-21 14:13:13 +0900

    ユーザーにnameカラムを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update!(user_params)
+      redirect_to edit_user_url, notice: 'ユーザー情報を更新しました。'
+    end
   end
 
   private
 
   def set_user
     @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
