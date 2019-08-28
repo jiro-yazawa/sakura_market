03d34b9bb98bc7cc665520d1854be25f85676453
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-08-29 07:07:32 +0900

    Productモデルに正常系のテストケースを1つ追加

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with name and price" do
+    product = Product.new(
+      name: "Apple",
+      image: nil,
+      price: 300,
+      description: nil,
+      hidden: nil,
+      position: nil
+    )
+
+    expect(product).to be_valid
+  end
 end
