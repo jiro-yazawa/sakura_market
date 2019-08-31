0990206cb1ba4a18822c1c12976231510c074c6a
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-09-01 08:41:29 +0900

    カートアイテム関連のルーティングをproductsに紐づくように修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root "products#index"
   devise_for :users, controllers: {sessions: "users/sessions", registrations: "users/registrations"}
   resource :cart, only: [:show]
-  resource :cart_item, only: [:create, :update, :destroy]
   resources :orders, only: [:index, :show, :new, :create]
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :cart_items, only: [:create, :update, :destroy]
+  end
   resource :user, only: [:edit, :update] do
     resource :address, only: [:edit, :update]
   end
