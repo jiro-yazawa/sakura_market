1096cfbcb4355515df82a91a6cab3947c052efae
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-09-01 17:59:07 +0900

    Productのspecを実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,8 @@ class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details
 
+  validates :name, presence: true
+  validates :price, presence: true
+
   scope :showable, -> { where(hidden: false) }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,4 +13,30 @@ RSpec.describe Product, type: :model do
 
     expect(product).to be_valid
   end
+
+  it "is invalid without name" do
+    product = Product.new(
+      name: nil,
+      image: nil,
+      price: 300,
+      description: nil,
+      hidden: nil,
+      position: nil
+    )
+
+    expect(product).to be_invalid
+  end
+
+  it "is invalid without price" do
+    product = Product.new(
+      name: "Apple",
+      image: nil,
+      price: nil,
+      description: nil,
+      hidden: nil,
+      position: nil
+    )
+
+    expect(product).to be_invalid
+  end
 end
