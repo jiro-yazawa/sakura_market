f0aa574b47c2eaa330c5b10b0956122b9a21f0d8
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-09-09 20:26:18 +0900

    料金計算のロジックを修正

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,17 +2,19 @@ class OrderDetail < ApplicationRecord
   belongs_to :order, dependent: :destroy
   belongs_to :product
 
-  def calculate_subtotal
-    self.subtotal = unit_price * quantity
-  rescue
-    self.subtotal = 0
-  end
-
   def set_order_detail_attributes(cart_item)
     self.product= cart_item.product
     self.unit_price = cart_item.product.price
     self.quantity = cart_item.quantity
-    calculate_subtotal
+    self.subtotal = calculate_subtotal
+  end
+
+  private
+
+  def calculate_subtotal
+    unit_price * quantity
+  rescue
+    0
   end
 
 end
