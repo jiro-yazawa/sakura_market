d4d24346fac924bcf65269716ad1f911c7f5d5d0
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-09-10 19:56:39 +0900

    Noteモデルのhas_many :commentsにdependentを追加

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :contents, presence: true
   mount_uploader :image, ImagesUploader
